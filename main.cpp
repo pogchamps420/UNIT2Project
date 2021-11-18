7402daf7730152f979565f51bee1556ec01f28e8
DELILAH <delilahhague@gmail.com>
2021-11-18 00:57:16 +0000

    add bare bones menu

--- main.cpp
+++ main.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
 
+void Menu() {
+
+    int option;
+
+     std::cout << std::endl << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << std::endl;
+     std::cout << std::endl << "        Main Menu           " << std::endl;
+     std::cout << std::endl << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << std::endl;
+
+     std::cout << std::endl << "Please select an option from 1-5: ";
+
+     std::cin >> option;
+
+     switch (option) {
+         case 1:
+            //Option1();
+            break;
+         case 2:
+            //Option2();
+            break;
+         case 3:
+            //Option3();
+            break;
+         case 4:
+            //Option4();
+            break;
+         case 5:
+            //Option5();
+            break;
+         default:
+            break;
+     }
+}
+
 int main() {
-    std::cout << "Joe mama" << std::endl;
+    Menu();
     return 0;
 }
