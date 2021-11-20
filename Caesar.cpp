f5d817038a4cf55659bf5be6502ad5bff54563a4
Delilah <43878546+Valkiyare@users.noreply.github.com>
2021-11-20 07:20:25 +0000

    create new file for crypto
    Decided to experiment with Caesar cipher. Codes a bit dogshit as I’m tired but I’ll update it later

---
+++ Caesar.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+
+int main()
+{
+   int option, n;
+   string message;
+   string shift;
+
+   std::cout << "Please enter a phrase to be encoded or decoded:\n";
+   getline(std::cin, message);
+
+   std::cout << "Please specify the shift amount:\n";
+   std::cin << n;
+
+   std::cout << "Type L for Left Shift or R for Right Shift:\n";
+   std::cin << shift;
+   
+   std::cout << "What would you like to do? :\n"; 
+   std::cout << "1 = Encryption.\n";
+   std::cout << "2 = Decryption.\n";
+   std::cin >> option;
+
+   switch(option)
+   {
+      case 1:
+         for(int i = 0; i < message.size(); i++)
+            if(shift == "L") {
+              message[i] = message[i] - n;
+            }
+            else if(shift == "R") {
+               message[i] = message[i] + n;
+            }
+            else {
+               std::cout << "Invalid shift option!\n";
+
+         cout << "The encrypted message: " << message;
+         break;
+
+
+      case 2:
+        for(int i = 0; i < message.size(); i++)
+            if(shift == "L") {
+              message[i] = message[i] - n;
+            }
+            else if(shift == "R") {
+               message[i] = message[i] + n;
+            }
+            else {
+               std::cout << "Invalid shift option!\n";
+
+         std::cout << "The decrypted message: " << message;
+         break;
+
+
+      default:
+         std::cout << "oopsy daisy you dumb motherfucker";
+   }
+   return 0;
+}
