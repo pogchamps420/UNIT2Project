d8944ccb761a63cf24123213d39eb1b083eb745d
Tise Olayinka <tiseolayinka@gmail.com>
2021-11-21 00:24:15 +0000

    test

--- main.cpp
+++ main.cpp
@@ -14,18 +14,23 @@ void Menu() {
 
      switch (option) {
          case 1:
+            std::cout << std::endl << "~~~~~~~~~~~~~~~ 1 ~~~~~~~~~~~~~\n" << std::endl; 
             //Option1();
             break;
          case 2:
+            std::cout << std::endl << "~~~~~~~~~~~~~~~ 2 ~~~~~~~~~~~~~\n" << std::endl;
             //Option2();
             break;
          case 3:
+            std::cout << std::endl << "~~~~~~~~~~~~~~~ 3 ~~~~~~~~~~~~~\n" << std::endl;
             //Option3();
             break;
          case 4:
+            std::cout << std::endl << "~~~~~~~~~~~~~~~ 4 ~~~~~~~~~~~~~\n" << std::endl;
             //Option4();
             break;
          case 5:
+            std::cout << std::endl << "~~~~~~~~~~~~~~~ 5 ~~~~~~~~~~~~~\n" << std::endl;
             //Option5();
             break;
          default:
@@ -34,6 +39,6 @@ void Menu() {
 }
 
 int main() {
-    Menu();
+    Menu(); // launch menu
     return 0;
 }
