f37ae3a65708eddedf1a696176bd6c3b92c7e24b
Rafal <rafal.kalisz05@gmail.com>
2021-11-23 01:55:42 +0000

    Prepare for implementing inherited menu class

--- rlcmatcher.cpp
+++ rlcmatcher.cpp
@@ -1,1 +1,12 @@
 #include "rlcmatcher.h"
+#include "menuprinter.h"
+#include <iostream>
+#include <vector>
+
+namespace rlc
+{
+	class RlcMenu : public Menu
+	{
+
+	};
+}
--- rlcmatcher.h
+++ rlcmatcher.h
@@ -1,5 +1,14 @@
 #ifndef RLCMATCHER_H
 #define RLCMATCHER_H
 
+#include "menuprinter.h"
+#include <string>
+#include <vector>
+namespace rlc
+{
+	class RlcMenu : public Menu
+	{
 
+	};
+}
 #endif
