8d702785a517eda009f454d9cd1051ed6516f025
Tise Olayinka <tiseolayinka@gmail.com>
2021-12-02 03:36:00 +0000

    test 1

---
+++ formula.cpp
@@ -0,0 +1,13 @@
+//  add a database to hold all formulas, formulas are retrieved by name
+#include "formula.h"
+
+Formula::Formula()
+	: _fname("Test"), _formula() {}
+Formula::Formula(std::string fname, TheFormula formula)
+	: _fname(fname), _formula() {}
+
+void Formula::set_fname(std::string fname) { _fname = fname; }
+void Formula::edit_formula(TheFormula formula) { _formula = formula; }
+
+std::string Formula::get_fname() { return _fname; }
+std::string Formula::get_f() { return _f; }
---
+++ initiate.h
@@ -0,0 +1,25 @@
+#ifndef INITIATE_H
+#define INITIATE_H
+
+#include <algorithm>
+#include <iostream>
+#include "formula.h"
+#include "module.h"
+#include "user.h"
+
+class Initiate {
+public:
+	Initiate();
+	Initiate(int Profile);
+
+	void set_profile(int _profile);
+	void defaultprofile();
+	void testprofile();
+
+	User get_user();
+
+private:
+	int _profile;
+	User _user;
+};
+#endif
