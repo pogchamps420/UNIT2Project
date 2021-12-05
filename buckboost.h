c90943940e7054f09eddbae0c1534596e5bd73fa
DELILAH <delilahhague@gmail.com>
2021-12-05 23:19:43 +0000

    commit buckboost stuff

---
+++ buckboost.h
@@ -0,0 +1,16 @@
+#ifndef BUCKBOOST_H
+#define BUCKBOOST_H 
+
+#include <iostream>
+#include <string>
+
+
+//class here pls, tie it all up
+namespace bbc {
+	class Buck {
+	public:
+
+	};
+}
+#endif
+
