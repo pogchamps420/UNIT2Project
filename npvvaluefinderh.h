05a98fa52afc0325b5e5bdb0cab18ac934a603ff
Rafal <rafal.kalisz05@gmail.com>
2021-12-10 00:34:53 +0000

    Implemented npv value page

---
+++ npvvaluefinderh.h
@@ -0,0 +1,17 @@
+#pragma once
+#ifndef NPVVALUEFINDER_H
+#define NPVVALUEFINDER_H
+
+#include "menu.h"
+#include "menuelement.h"
+#include "eseries.h"
+
+#include <vector>
+
+namespace npvfinder
+{
+	void RunNpvFinder();
+}
+
+#endif // !NPVVALUEFINDER_H
+
